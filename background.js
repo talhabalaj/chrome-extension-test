070c148366a71ad96e3d790777bd69aab3b5b27a
Talha Balaj <talhabalaj@hotmail.com>
2018-05-28 15:10:06 +0100

    updated the name

--- background.js
+++ background.js
@@ -4,7 +4,8 @@
 
 'use strict';
 
-
+const color = "#35434d !important";
+const secondaryColor = "#2a2a2a !important";
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
@@ -18,28 +19,49 @@ chrome.runtime.onInstalled.addListener(function () {
             if (loginPassword) {
               loginPassword.addEventListener('focus', () => {
                 loginPassword.setAttribute('type', 'password');
-              })
+              });
             }
-            
-            // Style change 
-            const color = 'red';
-            document.querySelector('.masthead').style.backgroundColor = color;
-            document.querySelector('.active').style.color = color;
-            document.querySelector('.nav-lang').style.backgroundColor = color;
-            document.querySelectorAll('.nav>li>a:hover, .nav>li>a:focus').forEach(
-              (each) => {
-                each.style.color = color;
-              }
-            )
-
-            document.body.style.fontFamily = 'True lies';
-            
-
           }
           window.onload = injectScript();
           `
         });
-      }  
+        chrome.tabs.insertCSS(tabId, {
+          code:`
+          body {
+            font-family: 'SF UI Text', sans-serif !important;
+          }
+          .nav-lang {
+            background-color: ${color};
+          }
+          .masthead {
+            background-color: ${color};
+          }
+          .active {
+            color: ${color};
+          }
+          .nav>li>a:hover, .nav>li>a:focus {
+            color: ${color};
+          }
+          #statistic fieldset:hover {
+            border-color: ${color};
+          }
+          .table>thead>tr>th {
+            color: ${color};
+          }
+          .text-primary {
+            color: ${color};
+          }
+          .text-warning {
+            color: ${color};
+          }
+          .btn-primary {
+            background-color: ${color};
+            border-color: ${color};
+          }
+
+          `
+        }); 
+      }
     }
   });
 });
